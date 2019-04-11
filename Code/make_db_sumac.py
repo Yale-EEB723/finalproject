4d01a056d10b7a431360a7ac0fcc45d6b480deee
Diego Ellis Soto <diego.ellis91@hotmail.com>
2019-04-11 15:58:35 -0400

    Changes a lot of things

---
+++ Code/make_db_sumac.py
@@ -0,0 +1,1 @@
+python -m sumac -d MAM -i Myotis -o Murina -minl 750 --cores 4 -g /Users/diegoellis/projects/development/Comparative_genomics/finalproject/Data/TLR_gene_9_myotis_lucifugus.FASTA -de 
